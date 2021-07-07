232f8243a2be3697b9732f6e9c3ebdab6136ac49
masonhsnow <masonhsnow@gmail.com>
2021-07-06 19:26:58 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'f6bbcc23-a2ac-4781-bb4e-a55e1e70fbd4';
+export const MODAL_CLOSE_SEC = 2.5;
